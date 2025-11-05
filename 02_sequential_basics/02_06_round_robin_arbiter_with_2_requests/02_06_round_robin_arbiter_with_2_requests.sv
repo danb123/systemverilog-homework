550a45028d456cc054f6b0e266b523b62edb622d
Dan Barrowman <danbarrowman@yahoo.com>
2025-11-04 19:55:51 -0800

    Add 02_06 and 02_07 solutions

--- 02_sequential_basics/02_06_round_robin_arbiter_with_2_requests/02_06_round_robin_arbiter_with_2_requests.sv
+++ 02_sequential_basics/02_06_round_robin_arbiter_with_2_requests/02_06_round_robin_arbiter_with_2_requests.sv
@@ -22,6 +22,56 @@ module round_robin_arbiter_with_2_requests
     // Example:
     // requests -> 01 00 10 11 11 00 11 00 11 11
     // grants   -> 01 00 10 01 10 00 01 00 10 01
+    //
+    // questions:
+    //  1. are grants expected to be immediate (combinatorial), or, clocks and
+    //  thus one cycle after requests? this is not clear.
+    //
+    logic granted_zero; // indicates if the previous grant was for request[0]
+    logic [1:0] grants_d;
 
+    // it is assumed that requests is already synchronized and is stable
+    // during the entire clock cycle, otherwise grants could be corrupted
+
+    // granted_zero is a flag that indicates if that last grant was request[0]
+    always_ff @(posedge clk) begin
+      if (rst) begin
+        granted_zero <= 0;
+      end else begin
+        if (grants == 2'b01) begin
+          granted_zero <= 1;
+        end else if (grants == 2'b10) begin
+          granted_zero <= 0;
+        end
+      end
+    end
+
+    // combinatorial version 1
+    always_comb begin
+      if (requests == 2'b11) begin
+        if (granted_zero) begin
+          grants_d = 2'b10;
+        end else begin
+          grants_d = 2'b01;
+        end
+      end else begin
+        grants_d = requests;
+      end
+    end
+    assign grants = grants_d;
+
+
+    // combinatorial version 2
+    // always_comb begin
+    //   if (requests == 2'b11) begin
+    //     if (granted_zero) begin
+    //       grants = 2'b10;
+    //     end else begin
+    //       grants = 2'b01;
+    //     end
+    //   end else begin
+    //     grants = requests;
+    //   end
+    // end
 
 endmodule
--- 02_sequential_basics/02_07_serial_to_parallel/02_07_serial_to_parallel.sv
+++ 02_sequential_basics/02_07_serial_to_parallel/02_07_serial_to_parallel.sv
@@ -27,5 +27,24 @@ module serial_to_parallel
     // Note:
     // Check the waveform diagram in the README for better understanding.
 
+    logic [$clog2(width) - 1: 0] bit_count;
+
+    // implement a bit counter and last indicator
+    always_ff @(posedge clk) begin
+      parallel_valid <= 0;
+      if (rst) begin
+        bit_count <= '0;
+        parallel_data <= '0;
+      end else if (serial_valid) begin
+        if (bit_count == (width - 1)) begin
+          parallel_valid <= 1;
+          bit_count <= '0;
+        end else begin
+          bit_count <= bit_count + 1;
+        end
+        parallel_data <= parallel_data >> 1;
+        parallel_data[7] <= serial_data;
+      end
+    end
 
 endmodule
