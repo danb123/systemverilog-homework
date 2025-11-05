85330335e2ff00abf404ed714bcedd781428e2c9
Dan Barrowman <danbarrowman@yahoo.com>
2025-11-04 21:57:15 -0800

    Add 02_08 solution

--- 02_sequential_basics/02_08_parallel_to_serial/02_08_parallel_to_serial.sv
+++ 02_sequential_basics/02_08_parallel_to_serial/02_08_parallel_to_serial.sv
@@ -29,5 +29,39 @@ module parallel_to_serial
     // Note:
     // Check the waveform diagram in the README for better understanding.
 
+    logic [$clog2(width) - 1: 0] bit_count;
+    logic [width - 1:0] shift_data;
+
+    // implement a bit counter
+    // started by parallel_valid and ends with bit_count = width - 1
+    always_ff @(posedge clk) begin
+      if (rst) begin
+        bit_count <= '0;
+      end else if (parallel_valid) begin
+        bit_count <= 1;
+      end else if (busy) begin
+        bit_count <= bit_count + 1;
+      end else if (bit_count == (width - 1)) begin
+        bit_count <= '0;
+      end
+    end
+
+    //
+    // implement a bit counter and shift data register
+    always_ff @(posedge clk) begin
+      if (rst) begin
+        shift_data <= '0;
+      end else if (parallel_valid) begin
+        shift_data <= parallel_data >> 1;
+      end else if (busy) begin
+        shift_data <= shift_data >> 1;
+      end else begin
+        shift_data <= 0;
+      end
+    end
+
+    assign busy = (bit_count != 0);
+    assign serial_data = (parallel_valid) ? parallel_data[0] : shift_data[0];
+    assign serial_valid = (parallel_valid | busy);
 
 endmodule
